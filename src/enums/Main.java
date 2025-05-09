1df39198d6519b8df2977e33c34e764115823a0f
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-05-09 06:02:09 +0100

    Add enums package to play with

--- src/enums/Main.java
+++ src/enums/Main.java
@@ -1,4 +1,31 @@
 package enums;
 
 public class Main {
+
+    public static void main(String[] args) {
+        MyEnum myEnumB = MyEnum.B;
+        System.out.printf("myEnumB's weight = %d%n", myEnumB.getWeight());
+
+        MyEnum myEnumA = MyEnum.A;
+        System.out.printf("myEnumA's weight = %d%n", myEnumA.getWeight());
+    }
+}
+
+enum MyEnum {
+    A, B(2), C;
+
+    private int weigh;
+
+    // Enum constructor is implicitly private
+    private MyEnum() {
+        this.weigh = 0;
+    }
+
+    private MyEnum(int weigh) {
+        this.weigh = weigh;
+    }
+
+    public int getWeight() {
+        return weigh;
+    }
 }
