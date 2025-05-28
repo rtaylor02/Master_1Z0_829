d009986ce80441f524e81270482d3922bf5a8268
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-05-28 18:04:35 +0100

    Start assignment 2

--- src/lambdaexpression/seankennedy/assignment.md
+++ src/lambdaexpression/seankennedy/assignment.md
@@ -1,3 +1,3 @@
 # Resource
-Assignment PDFs are available [here](C:\Users\mailb\OneDrive\My Documents\GitHub\Sean_Kennedy_AdvancedJavaProgramming\pdfs\j8\lambdas).
+Assignment PDFs are available [here](C:\Users\mailb\OneDrive\My Documents\GitHub\Sean_Kennedy_AdvancedJavaProgramming\pdfs\j8\lambdas\assignments).
 
--- src/lambdaexpression/seankennedy/assignment2/LambdaAndMethodReferences.java
+++ src/lambdaexpression/seankennedy/assignment2/LambdaAndMethodReferences.java
@@ -1,4 +1,30 @@
 package lambdaexpression.seankennedy.assignment2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdaAndMethodReferences {
+    public static void main(String[] args) {
+        AssigmentNumber1.staticMR();
+    }
+    private class AssigmentNumber1 {
+        private static void staticMR() {
+            List<Integer> integers = Arrays.asList(1, 2, 7, 4, 5);
+            System.out.println(integers);
+            System.out.println("============");
+            Consumer<List<Integer>> consumer = list -> Collections.sort(list);
+            consumer.accept(integers);
+            System.out.println(integers);
+
+            integers = Arrays.asList(1, 2, 7, 4, 5);
+            System.out.println(integers);
+            System.out.println("============");
+            consumer = Collections::sort;
+            consumer.accept(integers);
+            System.out.println(integers);
+
+        }
+    }
 }
