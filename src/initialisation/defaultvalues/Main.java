f9cd3d45ca035ae1832b21de5008f9f10814b36f
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-05-28 14:40:15 +0100

    Add sample code for  initialisation default values

---
+++ src/initialisation/defaultvalues/Main.java
@@ -0,0 +1,16 @@
+package initialisation.defaultvalues;
+
+public class Main {
+    static boolean b;
+    static int[] ia = new int[1];
+    static char ch;
+    static boolean[] ba = new boolean[1];
+    public static void main(String args[]) throws Exception{
+        var x = false;
+        if( b ){
+            x = ( ch == ia[ch]);
+        }
+        else x = ( ba[ch] = b );
+        System.out.println(x+" "+ba[ch]); // false false
+    }
+}
